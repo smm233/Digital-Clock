4be7209c34c8c4ae7971c7a860ea20169a476689
shil-mil <shyshy122303@gmail.com>
2025-03-21 14:04:25 +0800

    Time will update every second

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let hours = document.getElementById("hours");
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
 
-let currentTime = new Date();
+setInterval(()=>{
+    let currentTime = new Date();
 
-hours.innerHTML = currentTime.getHours();
-minutes.innerHTML = currentTime.getMinutes();
-seconds.innerHTML = currentTime.getSeconds();
+    hours.innerHTML = currentTime.getHours();
+    minutes.innerHTML = currentTime.getMinutes();
+    seconds.innerHTML = currentTime.getSeconds();
+}, 1000)
