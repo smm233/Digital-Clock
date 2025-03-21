7ac29aabb92553c4265a89808fb287c5bc017eca
shil-mil <shyshy122303@gmail.com>
2025-03-21 14:06:03 +0800

    Add 0 if time is one digit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let seconds = document.getElementById("seconds");
 setInterval(()=>{
     let currentTime = new Date();
 
-    hours.innerHTML = currentTime.getHours();
-    minutes.innerHTML = currentTime.getMinutes();
-    seconds.innerHTML = currentTime.getSeconds();
+    hours.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+    minutes.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+    seconds.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
 }, 1000)
